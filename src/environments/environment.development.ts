8b42bcbcd89973894c2fa4a0089c7f692fe16ee8
ianoxwell <ianoxwell@gmail.com>
2025-08-06 15:53:44 +1000

    feat: add bucket urls for public buckets

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   test: false,
+  workoutBucket: 'https://mimaktxujtbngbncgwqn.supabase.co/storage/v1/object/public/workout-images/',
+  exerciseBucket: 'https://mimaktxujtbngbncgwqn.supabase.co/storage/v1/object/public/exercises/',
 };
